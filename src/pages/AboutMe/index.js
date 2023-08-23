4a49851f11fa876996bd3d62d633385893c96036
Rute-r <rute_ke@hotmail.com>
2023-08-23 14:03:34 -0300

    aboutMe

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import styles from './AboutMe.module.css';
+import aboutMePhoto from 'assets/img/aboutme-photo.jpg';
 
 export default function AboutMe() {
-  return <div>AboutMe</div>;
+  return (
+    <article className={styles.aboutMeContainer}>
+      <div className={styles.postContentContainer}>
+        <h3 className={styles.subtitle}>Hello, my name is Rute!</h3>
+        <img className={styles.aboutMePhoto} src={aboutMePhoto} alt="Foto de Rute sorrindo" />
+        <p className={styles.paragraph}>Hi, hrurefjnl, skwdjebf dhbwrjdn </p>
+      </div>
+    </article>
+  );
 }
